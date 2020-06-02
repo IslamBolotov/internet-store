5eef42e9d35a410bc68abc1dccfef43b93dea2da
Bektur Tologonov <tbektur7@gmail.com>
2020-06-02 21:37:48 +0600

    dev2

--- src/App.js
+++ src/App.js
@@ -3,7 +3,11 @@ import React from 'react';
 import './App.css';
 import Router from './Router';
 
-const App = () => <Router />
+const App = () => {
+
+    return (<h1>dev2</h1>)}
+
+
 
 
 export default App;
